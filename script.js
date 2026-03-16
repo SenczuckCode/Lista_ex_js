8c9672a3c92914329ce015e59b6d01ecc32977a8
SenczuckCode <lucas_senczuck@estudante.sesisenai.org.br>
2026-03-15 21:16:55 -0300

    ADD- 2 e 3 feitas

--- script.js
+++ script.js
@@ -21,28 +21,59 @@
 // }
 // somar(15,17);
 
-function somarNumeros() {
+// function somarNumeros() {
 
-    let soma = 0;
-    let continuar = true;
+//     let soma = 0;
+//     let continuar = true;
 
 
-    while (continuar) {
-        let numero = parseFloat(prompt("Digite um número (ou digite '0' para encerrar) :"));
+//     while (continuar) {
+//         let numero = parseFloat(prompt("Digite um número (ou digite '0' para encerrar) :"));
 
-        //soma += numero;
+//         //soma += numero;
 
-        if (isNaN(numero)) {
-            alert("Por favor, digite um número válido!")
-        } else {
-            soma += numero;
-            console.log(soma);
-        }
+//         if (isNaN(numero)) {
+//             alert("Por favor, digite um número válido!")
+//         } else {
+//             soma += numero;
+//             console.log(soma);
+//         }
 
-        continuar = confirm("deseja adicionar mais um número.");
-    }
-    alert("A Soma dos números é: " + soma);
-    console.log("A soma dos números é: " + soma);
-}
+//         continuar = confirm("deseja adicionar mais um número.");
+//     }
+//     alert("A Soma dos números é: " + soma);
+//     console.log("A soma dos números é: " + soma);
+// }
 
-somarNumeros();
+// somarNumeros();
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i);
+
+// }
+
+// console.log("Lançamento do foguete em + " + i);
+
+// let ganho_total = 0;
+// let gastos_total = 0;
+
+// for (let i = 0; i < 12; i++){
+//     let ganho = parseFloat(prompt("Digite o ganho do mês " + (i + 1) + ":"));
+//     let gasto = parseFloat(prompt("Digite o gasto do mês " + (i + 1) + ":"));
+
+//     ganho_total += ganho;   
+//     gastos_total += gasto;  
+
+// }
+
+// let saldo_final = ganho_total - gastos_total;
+
+// console.log("Ganho total do ano: " + ganho_total);
+// console.log("Gastos total do ano: " + gastos_total);
+// console.log("Saldo final do ano: " + saldo_final);
+
+// if (saldo_final > 0) {
+//     console.log("Você teve lucro");
+// } else {
+//     console.log("Você não teve lucro");
+// }
