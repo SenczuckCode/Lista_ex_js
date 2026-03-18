f3cbfa633579a3b0fb577cbf935f2ced1163d82c
SenczuckCode <lucas_senczuck@estudante.sesisenai.org.br>
2026-03-17 21:13:51 -0300

    ADD- 11 feita

--- script.js
+++ script.js
@@ -177,3 +177,24 @@
 // console.log("Salário bruto:", salario);
 // console.log("Desconto INSS:", desconto);
 // console.log("Salário líquido:", liquido);
+
+
+// let salario = parseFloat(prompt("Digite o salário:"));
+
+// let taxa;
+
+// if salario <= 1000) {
+//     taxa = 0.08;
+// } else if (salario <= 1500) {
+//     taxa = 0.085;
+// } else {
+//     taxa = 0.09;
+// }
+
+// let desconto = salario * taxa;  
+// let liquido = salario - desconto;   
+
+// console.log("Salário:", salario);
+// console.log("Taxa aplicada:", taxa * 100 + "%");
+// console.log("Desconto:", desconto);
+// console.log("Salário líquido:", liquido);
